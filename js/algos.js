4335318cf2866f20bf7817dbb200073b97bdcb94
Khadija dh <khadija.dhieb@le-campus-numerique.fr>
2020-11-09 16:40:21 +0100

    mise en place de l'équation de calcul de distance dans distanceFromGrenoble(city)

--- js/algos.js
+++ js/algos.js
@@ -1,24 +1,35 @@
 // Converts from degrees to radians.
-Number.prototype.toRadians = function() {
+Number.prototype.toRadians = function () {
   return this * Math.PI / 180;
 };
 
 
 // Calculates the distance between Grenoble and the given city
-function distanceFromGrenoble(city)
-{
+function distanceFromGrenoble(city) {
   console.log("distanceFromGrenoble - implement me !");
   var GrenobleLat = 45.166667;
   var GrenobleLong = 5.716667;
 
-  return 1;
+  const R = 6371e3; // metres
+  const φ1 = GrenobleLat * Math.PI / 180; // φ, λ in radians
+  const φ2 = city.latitude * Math.PI / 180;
+  const Δφ = (city.latitude - GrenobleLat) * Math.PI / 180;
+  const Δλ = (city.longitude - GrenobleLong) * Math.PI / 180;
+
+  const a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+    Math.cos(φ1) * Math.cos(φ2) *
+    Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+  const d = (R * c)/1000 ; // in klm
+
+  return d;
 }
 
 // Swap 2 values in array csvData
 // i is the index of the first city
 // j is the index of the second city
-function swap(i,j)
-{
+function swap(i, j) {
   displayBuffer.push(['swap', i, j]); // Do not delete this line (for display)
   console.log("swap - implement me !");
 
@@ -27,65 +38,54 @@ function swap(i,j)
 // Returns true if city with index i in csvData is closer to Grenoble than city with index j
 // i is the index of the first city
 // j is the index of the second city
-function isLess(i, j)
-{
+function isLess(i, j) {
   displayBuffer.push(['compare', i, j]); // Do not delete this line (for display)
   console.log("isLess - implement me !");
 }
 
 
-function insertsort()
-{
+function insertsort() {
   console.log("insertsort - implement me !");
 }
 
-function selectionsort()
-{
+function selectionsort() {
   console.log("selectionsort - implement me !");
 }
 
-function bubblesort()
-{
+function bubblesort() {
   console.log("bubblesort - implement me !");
 }
 
-function shellsort()
-{
+function shellsort() {
   console.log("shellsort - implement me !");
 }
 
-function mergesort()
-{
+function mergesort() {
   console.log("mergesort - implement me !");
 }
 
-function heapsort()
-{
+function heapsort() {
   console.log("heapsort - implement me !");
 }
 
-function quicksort()
-{
+function quicksort() {
   console.log("quicksort - implement me !");
 }
-function quick3sort()
-{
+function quick3sort() {
   console.log("quick3sort - implement me !");
 }
 
 
-function sort(algo)
-{
-  switch (algo)
-  {
-    case 'insert': insertsort();break;
-    case 'select': selectionsort();break;
-    case 'bubble': bubblesort();break;
-    case 'shell': shellsort();break;
-    case 'merge': mergesort();break;
-    case 'heap': heapsort();break;
-    case 'quick': quicksort();break;
-    case 'quick3': quick3sort();break;
+function sort(algo) {
+  switch (algo) {
+    case 'insert': insertsort(); break;
+    case 'select': selectionsort(); break;
+    case 'bubble': bubblesort(); break;
+    case 'shell': shellsort(); break;
+    case 'merge': mergesort(); break;
+    case 'heap': heapsort(); break;
+    case 'quick': quicksort(); break;
+    case 'quick3': quick3sort(); break;
     default: throw 'Invalid algorithm ' + algo;
   }
 }
